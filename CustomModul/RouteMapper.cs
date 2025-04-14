697e7b04842463aecbaa2c7120cfccab3ab50798
bendeguzmiskey <bendeguz.miskey@gmail.com>
2025-04-14 19:03:56 +0200

    RouteMapper.cs added

---
+++ CustomModul/RouteMapper.cs
@@ -0,0 +1,17 @@
+﻿using DotNetNuke.Web.Api;
+
+namespace CustomModul.CustomModul
+{
+    public class RouteMapper : IServiceRouteMapper
+    {
+        public void RegisterRoutes(IMapRoute mapRouteManager)
+        {
+            mapRouteManager.MapHttpRoute(
+                moduleFolderName: "CustomModul", // Must match the folder name in your .dnn manifest
+                routeName: "default", // A unique name for this route
+                url: "{controller}/{action}", // URL pattern: [ControllerName]/[ActionName]
+                namespaces: new[] { "CustomModul.CustomModul.Controllers" } // Namespace of your controllers
+            );
+        }
+    }
+}
